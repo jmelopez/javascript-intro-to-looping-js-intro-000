11d4d67b63f73c493841f2c1bcbc8507b61f8ab3
Jamie Lopez <jmelopez@outlook.com>
2020-09-10 00:28:44 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,19 @@
+function forLoop(array) {
+    for (let i = 0; i < 25; i++) {
+        array.push(`I am ${i} strange ${i == 1 ? 'loop' : 'loops'}.`)
+    }
+    return array;
+}
+
+function whileLoop(n) {
+  while(n > 0) {
+    console.log(--n)
+  }
+  return 'done'
+}
+
+function doWhileLoop(num) {
+  do {
+    console.log('I run once regardless.');
+  } while (numb =+1)
+}
